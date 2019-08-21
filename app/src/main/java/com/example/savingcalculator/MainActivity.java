185f076b65f7d6d42603acc24a985dc687cbda05
chalinor <chalinor.baliuag@weltec.ac.nz>
2019-08-21 16:29:49 +1200

    added some functions for the buttons.

--- app/src/main/java/com/example/savingcalculator/MainActivity.java
+++ app/src/main/java/com/example/savingcalculator/MainActivity.java
@@ -2,12 +2,48 @@ package com.example.savingcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnCalculate, btnComputeSum;
+    private EditText num1, num2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCalculate = findViewById(R.id.btnCompute);
+        btnComputeSum = findViewById(R.id.btnSum);
+        num1 = findViewById(R.id.editTextNum1);
+        num2 = findViewById(R.id.editTextNum2);
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You clicked compute", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //this is for the Compute Sum button
+        btnComputeSum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Sum = " + add(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    } //end of onCreate() method
+
+    public int add(){
+        int n1 = Integer.parseInt(num1.getText().toString());
+        int n2 = Integer.parseInt(num2.getText().toString());
+        int sum = n1 + n2;
+        return sum;
     }
 }
---
+++ app/src/main/java/com/example/savingcalculator/Person.java
@@ -0,0 +1,47 @@
+package com.example.savingcalculator;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int id;
+
+    public Person(String firstName, String lastName, int id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
